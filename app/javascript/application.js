370cb0f9f49b1bcdfb476efa7b527b3df39b36c5
eNeville <17412822+eNeville@users.noreply.github.com>
2022-12-03 17:40:58 -0400

    Add uppy example to test uppy js object imported and available

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,7 @@ import 'controllers';
 
 import 'bootstrap';
 import 'uppy';
+
+var uppy = new Uppy.Uppy();
+uppy.use(Uppy.DragDrop, { target: '#drag-drop-area' });
+uppy.use(Uppy.Tus, { endpoint: 'https://tusd.tusdemo.net/files/' });
